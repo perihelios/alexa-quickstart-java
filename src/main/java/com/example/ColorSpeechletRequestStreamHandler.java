501ac3c897ce1c41ee3773a2afff46825c97ac3a
Jonathan Cottrill <github@jonathancottrill.net>
2016-12-14 10:45:50 -0600

    Add Apache 2.0 license files/comment headers

--- src/main/java/com/example/ColorSpeechletRequestStreamHandler.java
+++ src/main/java/com/example/ColorSpeechletRequestStreamHandler.java
@@ -1,3 +1,18 @@
+/*
+ Copyright 2016, Perihelios LLC
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
 package com.example;
 
 import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
@@ -9,7 +24,6 @@ import java.util.Properties;
 
 import static java.util.Collections.singleton;
 
-@SuppressWarnings("unused")
 public class ColorSpeechletRequestStreamHandler extends SpeechletRequestStreamHandler {
 	private static final String PROPERTIES_FILE = "app-id.properties";
 
